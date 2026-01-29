53690031c04f587cc1eeadbc19c04b097513c01c
Yead191 <yead191@gmail.com>
2026-01-29 20:18:30 +0600

    form completed

--- src/feature/home/sections/banner/Banner.tsx
+++ src/feature/home/sections/banner/Banner.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import BannerLeft from "./BannerLeft";
 import BannerRight from "./BannerRight";
+import Image from "next/image";
 
 export default function Banner() {
   return (
-    <div className="relative min-h-screen pt-24 lg:pt-44 pb-12 overflow-hidden">
+    <div className="relative min-h-screen pt-24 lg:pt-44 pb-12 lg:pb-[180px] overflow-hidden">
       {/* Background Overlay */}
       <div
         className="absolute inset-0 z-0 opacity-40 pointer-events-none"
@@ -24,6 +25,13 @@ export default function Banner() {
           <BannerRight />
         </div>
       </div>
+      <Image
+        src={"/assets/images/home/line.png"}
+        alt="line"
+        width={600}
+        height={600}
+        className="absolute bottom-12 -right-60 w-contain h-auto"
+      />
     </div>
   );
 }
